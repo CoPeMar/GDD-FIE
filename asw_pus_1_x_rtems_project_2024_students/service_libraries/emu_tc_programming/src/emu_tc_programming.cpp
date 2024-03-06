ac38b51f7dd4258acb294c4a96c266edb84368a7
CoPeMar <marco.personat@edu.uah.es>
2024-03-06 18:43:11 +0100

    Práctica 2 actualizada
    Hemos cambiado los archivos con TODO Fixed

--- asw_pus_1_x_rtems_project_2024_students/service_libraries/emu_tc_programming/src/emu_tc_programming.cpp
+++ asw_pus_1_x_rtems_project_2024_students/service_libraries/emu_tc_programming/src/emu_tc_programming.cpp
@@ -15,15 +15,15 @@
 #define FT_SOLO_EPD_ICU_SERV_9_0020
 
 
-//TODO
-//#define FT_SOLO_EPD_ICU_SERV_1_REJECTED_TC_0030
+//TODO Fixed
+#define FT_SOLO_EPD_ICU_SERV_1_REJECTED_TC_0030
 
 
 
 #ifdef FT_SOLO_EPD_ICU_SERV_17_0010
 
 EmuGSS_TCProgram17_1 prog_FT_0010_step_0(UNITIME_AFTER_POWER_ON + 6,
-					"FT_SOLO_EPD_ICU_SERV_17_0010 step 0, Connection Test");
+					"FT_SOLO_EPD_ICU_SERV_17_0010 step 0, Connection Test"); //Parámetros para el constructor
 
 
 #endif
@@ -34,12 +34,12 @@ EmuGSS_TCProgram17_1 prog_FT_0010_step_0(UNITIME_AFTER_POWER_ON + 6,
 
 EmuGSS_TCProgram9_129 prog_FT_0020_step_0(UNITIME_AFTER_POWER_ON + 10 ,
 		"FT_SOLO_EPD_ICU_SERV_9_0020 step 0, Update UniTime Test",
-		FT_SOLO_EPD_ICU_SERV_9_0020_TIME);
+		FT_SOLO_EPD_ICU_SERV_9_0020_TIME); //Parámetros para el constructor
 
 
 
 #endif
-/*
+
 #ifdef FT_SOLO_EPD_ICU_SERV_1_REJECTED_TC_0030
 
 
@@ -49,5 +49,5 @@ EmuGSS_TCProgram17_3 prog_FT_0030_step_0(FT_SOLO_EPD_ICU_SERV_1_0030_TIME,
 		"FT_SOLO_EPD_ICU_SERV_1_0030 step 0, Rejected TC",0xFF);
 
 #endif
-*/
+
 
